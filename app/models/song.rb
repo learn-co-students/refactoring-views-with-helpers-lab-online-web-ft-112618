22ef2ca6318dd81c547f281ee7305b34bb6ebeea
Molly McCarron <mccarronmollye@gmail.com>
2019-02-17 20:18:49 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
   end
 
-  def artist_name=(name)
+  def artist_name=(name) #goal of code is for Song.artist to be a thing
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
